0e825b28fd9b7331ee93c667024233d2d3a41cd1
Maya Zain El-Din <mayzaineldin@gmail.com>
2023-02-17 11:23:51 -0800

    m

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x = '0';
+
+	while (x <= '9')
+	{
+		putchar(x);
+		x++;
+	}
+	
+	x = 'a';
+
+	while (x < 'g')
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
