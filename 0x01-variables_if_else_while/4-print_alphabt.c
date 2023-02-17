cdaf4bb2c2252e73124f3df2a4e0a74fad0acd79
Maya Zain El-Din <mayzaineldin@gmail.com>
2023-02-17 10:50:16 -0800

    m

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		if (x != 'q' && x != 'e')
+			putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
